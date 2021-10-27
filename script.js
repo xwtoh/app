6cf0686d6085ca58c805aab5c26316b4f71c7268
xwtoh <55676405+xwtoh@users.noreply.github.com>
2021-10-27 17:33:47 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,48 @@
+'use strict';
 
-      $(document).ready(function(){
-            jQuery.fx.off = true;
- 
+const CSS = `
+body *, body *::before, body *::after {
+  /*CSS transition*/
+  transition: 0.01ms !important;
+  transition-delay: 0ms !important;
+  /*CSS animations*/
+  animation-delay: 0ms !important;
+  animation-duration: 0ms !important;
+ }
+`;
+
+let playTransition = false;
+//init
+toogleTransition();
+
+browser.browserAction.onClicked.addListener(handleClick);
+browser.webNavigation.onCommitted.addListener(handeTabUpdate)
+
+
+function handleClick() {
+    playTransition = !playTransition;
+    toogleTransition();
+}
+
+function handeTabUpdate(details) {
+	// @todo use details.frameId to inject in the specific updated frame
+    if (playTransition === false) {
+        disableTabTransition(details.tabId);
+    }
+}
+
+
+function disableAllTabsTransition() {
+    browser.tabs.query({}).then((tabs) => {
+        for (let tab of tabs) {
+            disableTabTransition(tab);
+        }
+    });
+}
+
+function disableTabTransition(tab) {
+    browser.tabs.insertCSS(tab.id, { code: CSS, cssOrigin: 'user', allFrames: true });
+}
+
+
+window.onload = disableAllTabsTransition;
